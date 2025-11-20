3351a82183a6e6942932ea955daf5d73a4b71660
corndev <corndev@mail.ru>
2025-11-20 12:37:52 +0300

    bugs

--- game/other/agents.gd
+++ game/other/agents.gd
@@ -1,13 +1,16 @@
 extends Node2D
 
+@export var agent : PackedScene
+@export var world : TileMapLayer
+@export var r_vision : TileMapLayer
+@export var c_vision : TileMapLayer
 
 signal next_turn(index: int)
 #manages turn order
 func _ready() -> void:
-	spawn_agents()
 	for child : Agent in get_children():
 		child.finished_turn.connect(_on_child_fished_turn)
-	get_child(0).active = true
+	
 
 func _on_child_fished_turn(index : int):
 	#await get_tree().process_frame
@@ -19,5 +22,23 @@ func _on_child_fished_turn(index : int):
 		next_turn.emit(index + 1)
 		get_child(index + 1).set_deferred("active",true)
 
-func spawn_agents():
-	pass
+func spawn_agents(runners : int, chasers : int):
+	for i in runners:
+		print("r")
+		var runner : Agent = agent.instantiate()
+		runner.character = "RUNNER"
+		runner.world = world
+		runner.vision = r_vision
+		var pos = Vector2i(randi_range(2,world.get_used_rect().size.x-2),randi_range(2,world.get_used_rect().size.y-2))
+		runner.global_position = world.local_to_map(pos)
+		add_child(runner)
+	for i in chasers:
+		print("r")
+		var chaser : Agent = agent.instantiate()
+		chaser.character = "CHASER"
+		chaser.world = world
+		chaser.vision = r_vision
+		var pos = Vector2i(randi_range(2,world.get_used_rect().size.x-2),randi_range(2,world.get_used_rect().size.y-2))
+		chaser.global_position = world.local_to_map(pos)
+		add_child(chaser)
+	get_child(0).active = true
--- game/world/label.gd
+++ game/world/label.gd
@@ -4,6 +4,8 @@ extends Label
 
 func _ready() -> void:
 	agents.next_turn.connect(change_text)
+	change_text(0)
 
 func change_text(index : int):
+	if get_child_count() == 0: return
 	text = "turn: " + agents.get_child(index).character + " " + str(index)
