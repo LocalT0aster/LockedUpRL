0a2466a655c61f8abde607d7d165a87220ea5821
corndev <corndev@mail.ru>
2025-11-21 20:38:33 +0300

    ses

--- game/vision_tilemap.gd
+++ game/vision_tilemap.gd
@@ -5,3 +5,4 @@ func update_vision(world : TileMapLayer, rect: Rect2i):
 		for j in rect.size.y:
 			var cell_coords = world.get_cell_atlas_coords(Vector2i(i,j) + rect.position)
 			set_cell(Vector2i(i,j) + rect.position, 0, cell_coords,0)
+	Global.get_state(world, rect)
